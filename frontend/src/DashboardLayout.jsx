70bdff240870c35c630131566804036aacd37609
Lucas Brennand <lucasbrennand.barbosa@gmail.com>
2025-09-14 16:31:57 -0300

    Revert "Added sales graphs"
    This reverts commit 566636c40f0268d1900e2fe9561039bc9945eed2.

--- frontend/src/DashboardLayout.jsx
+++ frontend/src/DashboardLayout.jsx
@@ -65,6 +65,17 @@ function DashboardLayout({ username, onLogout }) {
 
           <li>
             <NavLink
+              to="/dashboard/registrar-venda" // Nova rota
+              className={({ isActive }) =>
+                isActive ? `${linkClass} ${activeLinkClass}` : linkClass
+              }
+            >
+              Registrar Venda
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
               to="/dashboard/inserir-vendas" // Nova rota
               className={({ isActive }) =>
                 isActive ? `${linkClass} ${activeLinkClass}` : linkClass
